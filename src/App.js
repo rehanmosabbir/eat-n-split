18cf1d565aac0c6cf9b2c38438ae550346431a81
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-12 10:29:40 +0600

    add FormAddFriend and Button component

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
     </div>
   );
@@ -56,7 +58,23 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👯Friend name</label>
+      <input type="text" />
+      <label>🖼️Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
   );
 }
