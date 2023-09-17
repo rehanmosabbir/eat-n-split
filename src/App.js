d72b59289a93ef740495f4ae2d750c0af22b0249
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-17 10:44:51 +0600

    add key to FormSplitBill component

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBil}
+          key={selectedFriend.id}
         />
       )}
     </div>
